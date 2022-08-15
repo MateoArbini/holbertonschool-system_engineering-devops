abb474c0ffd74b8af97f9e5c1baaeda4167148df
MateoArbini <mateoarbini1998@gmail.com>
2022-08-14 17:45:09 -0700

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,10 +3,10 @@
 
 file_line { 'Identity file':
   ensure => 'present'
-  path => '/etc/ssh/ssh_config',
-  line => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 file_line { 'disable password':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
