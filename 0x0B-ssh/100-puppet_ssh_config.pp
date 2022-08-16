47d8d83fe1307a8ca57a7e0b8dae1b457b85e4d0
MateoArbini <mateoarbini1998@gmail.com>
2022-08-16 12:25:01 -0700

    task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 #making a puppet file to change our config. The files should be created in
 #the same way as we created in the previous project
-
 file_line { 'Identity file':
-  ensure => 'present'
+  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 file_line { 'disable password login':
-  path => '/etc/ssh/ssh_config',
-  line => '    PasswordAuthentication no',
+    path    => '/etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
 }
