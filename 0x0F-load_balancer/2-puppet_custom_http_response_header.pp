6dcd790b8ae22c084606c19e7770f7014c9c2748
Mateo <mateoarbini1998@gmail.com>
2022-08-22 11:09:24 -0700

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -28,7 +28,7 @@
 #}
 exec {'master command':
     command => sudo apt update; sudo apt-get install -y nginx;
-               sudo sed -i "/listen 80 default_server/a add_header X-Served-By '$HOSTNAME';" /etc/nginx/sites-enabled/default;
+               sudo sed -i "/listen 80 default_server/a add_header X-Served-By '${HOSTNAME}';" /etc/nginx/sites-enabled/default;
                sudo service nginx restart,
     provider => shell,
 }
