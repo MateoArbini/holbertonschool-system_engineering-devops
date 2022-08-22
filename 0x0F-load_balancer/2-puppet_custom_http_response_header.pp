eb813658aea6bbbba285696794f591c30a60ab6c
Mateo <mateoarbini1998@gmail.com>
2022-08-22 11:07:43 -0700

    task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,28 +1,34 @@
 #Automatization of tasks, but this time using puppet
-
+#
 #Execution
-exec {'update':
-    command  => 'sudo apt update',
-    provider => shell,
-}
-
+#exec {'update':
+#    command  => 'sudo apt update',
+#    provider => shell,
+#}
+#
 #Nginx Install
-exec {'nginx':
-    command => sudo apt-get install -y nginx,
-    provider => shell,
-}
-
+#exec {'nginx':
+#    command => sudo apt-get install -y nginx,
+#    provider => shell,
+#}
+#
 #Trying to modify the header
-file_line {'custom header':
-    ensure => present,
-    path   => '/etc/nginx/sites-available/default',
-    after  => 'server_name _;',
-    line   => '    location /{
-    add_header X-Served-By \$HOSTNAME;',
-    },
-}
-
-exec {'restart':
-    command  => 'sudo service nginx restart',
+#file_line {'custom header':
+#    ensure => present,
+#    path   => '/etc/nginx/sites-available/default',
+#    after  => 'server_name _;',
+#    line   => '    location /{
+#    add_header X-Served-By \$HOSTNAME;',
+#    },
+#}
+#
+#exec {'restart':
+#    command  => 'sudo service nginx restart',
+#    provider => shell,
+#}
+exec {'master command':
+    command => sudo apt update; sudo apt-get install -y nginx;
+               sudo sed -i "/listen 80 default_server/a add_header X-Served-By '$HOSTNAME';" /etc/nginx/sites-enabled/default;
+               sudo service nginx restart,
     provider => shell,
 }
