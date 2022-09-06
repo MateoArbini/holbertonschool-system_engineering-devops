60a1f10b82ce9cf9ac252dbb504b67ed7ef84212
Mateo <mateoarbini1998@gmail.com>
2022-09-06 20:10:17 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+Using what you did in the task #0, extend your Python script to export
+data in the CSV format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+'''
+
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        eID = argv[1]
+        # Users is a DICTIONARY
+        users = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(eID))
+        users = users.json()
+        # Tasks is a LIST
+        x = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(eID))
+        x = x.json()
+
+        # Here I will obtain the task name and the status
+        #for field in x:
+        #    print('"{}" - "{}"'.format(field['title'], field['completed']))
+        #for field in users:
+        #    print('"{}" - "{}"'.format(users.get('id'), users.get('username')))
+
+        with open('USER_ID.csv', 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+            for field in x:
+                row = [users.get('id'), users.get('username'), field['completed'], field['title']]
+                writer.writerow(row)
+        f.close()
+                
+    except Exception as e:
+        print(e)
