668977f63dbe562af5f0d07cb43e2cd1cc2b92ee
MateoArbini <mateoarbini1998@gmail.com>
2022-09-22 16:03:09 +0000

    Task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,3 @@
-exec { 'apt-get update':
-  command  => 'sudo apt-get -y update',
-  provider => shell,
-  before   => Exec['InstallNginx'],
+exec { 'fix':
+  command  => "/usr/bin/env sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php"
 }
-sed -i s/127.0.0.1/127.0.0.2/ ~/hosts.new
