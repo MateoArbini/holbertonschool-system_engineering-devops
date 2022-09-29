7599fda2e69c9357f97e9223a02f0e0435d3ab06
MateoArbini <mateoarbini1998@gmail.com>
2022-09-29 12:45:54 +0000

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,8 @@
 # Command to fix error
 exec {'fix':
-    command => '/usr/bin/env/sudo sed -i 's/15/2000/g' /etc/default/nginx' }
+    command => "/usr/bin/env sudo sed -i 's/15/2000/g' /etc/default/nginx"
+	}
 exec {'restart':
-    command => 'sudo service nginx restart' }
+    command => 'sudo service nginx restart',
+    provider => 'shell'
+	}
